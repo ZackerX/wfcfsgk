1b8eb225141112d67082a7325e5bc1a03e8fced6
zhaoxuan <1257112185@qq.com>
2018-11-24 11:03:05 +0800

    no message

--- .tea/entryFiles-production/config$.js
+++ .tea/entryFiles-production/config$.js
@@ -3,7 +3,7 @@ const g = typeof global !== 'undefined' ? global : self;
 g.appXRuntimeConfig = {};
 g.appXAppJson = {
   "app": {
-    "$homepage": "page/component/index"
+    "$homepage": "page/fsgk/home/home"
   },
   "page/API/option-menu/option-menu": {
     "optionMenu": {
--- page/fsgk/fsDetail/fsDetail.js
+++ page/fsgk/fsDetail/fsDetail.js
@@ -23,17 +23,52 @@ Page({
       arrIndex: e.detail.value,
     });
   },
-  showActionSheet() {
+  showMonitorVideo() {
+    dd.chooseVideo({
+      sourceType: ['camera'],
+      maxDuration: 60,
+      success:(res)=> {
+        console.log(res.apFilePath)
+      },
+      fail: (err)=> {
+        console.log(err)
+      }
+    })
+    
+  },
+  showHistroyDetail() {
     dd.showActionSheet({
       title: '历史记录选择',
       items: ['2018/11/16 06:00:00', '2018/11/16 04:00:00', '2018/11/16 02:00:00'],
       cancelButtonText: '取消',
-      success: (res) => {
-        const btn = res.index === -1 ? '取消' : '第' + res.index + '个';
-        dd.alert({
-          title: `你点了${btn}按钮`
-        });
+      //success: (res) => {
+      //  const btn = res.index === -1 ? '取消' : '第' + res.index + '个';
+      //  dd.alert({
+      //    title: `你点了${btn}按钮`
+      //  });
+      //},
+    });
+    
+  },
+  showEmergencyCall(){
+    dd.showCallMenu({
+      phoneNumber: '15968498652', // 期望拨打的电话号码
+      code: '+86', // 国家代号，中国是+86
+      showDingCall: true, // 是否显示钉钉电话
+      success:function(res){   
+        console.log('成功');
       },
+      fail:function(err){
+        console.log('失败'+err);
+      }
+    });
+  },
+  doWatchClock(e) {
+    dd.vibrate({
+      success: (res) => {
+        console.log(e.data);
+        dd.alert({ title: '打卡时间 2018/11/16 09:00:00'});
+      }
     });
   },
 });
